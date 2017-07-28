5fb11650929a6c700555ee237e77e38c730c75b3
AleksandrF <alxfro@yandex.ru>
2017-07-28 16:45:48 +0300

    Upload to Amazon S3 sql.tar.gz

--- src/backupaws.sh
+++ src/backupaws.sh
@@ -12,3 +12,16 @@ echo "mysqldump to /tmp/$BACKUP_NAME ...."
 mysqldump -u root -p123 -B db_yupe -h 172.26.0.2  | gzip > /tmp/$BACKUP_NAME
 ls -la /tmp
 echo Backup $BACKUP_NAME created!
+echo "Create bucket if not exist"
+BUCKET_EXIST=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
+if [ $BUCKET_EXIST -eq 0 ];
+then
+  aws s3 mb s3://$S3_BUCKET_NAME
+fi
+echo "Upload to bucket..."
+# Upload the backup to S3 with timestamp
+#aws s3 --region $AWS_DEFAULT_REGION cp /tmp/$BACKUP_NAME s3://$S3_BUCKET_NAME/$S3_FOLDER/$BACKUP_NAME
+aws s3 --region $AWS_DEFAULT_REGION cp /tmp/$BACKUP_NAME s3://$S3_BUCKET_NAME/test/$BACKUP_NAME
+
+# Clean up
+rm /tmp/$BACKUP_NAME
